0210d20da094be1b1cf2e28e1be589a0050c05c4
Vivien Henry <vivien.henry@inductivebrain.fr>
2020-09-20 11:47:15 +0200

    drop support for py2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["enum34"]
+requirements = [ ]
 
 setup_requirements = [ ]
 
@@ -27,13 +27,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Serialize and deserialize data from a buffer",
     entry_points={
